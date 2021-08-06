0d4226218a7412024309e025c493d8c670d51541
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-08-07 00:41:20 +0600

    input parameter error

---
+++ input-error.js
@@ -0,0 +1,21 @@
+// addition
+
+function addition(num1, num2) {
+    const addResult = num1 + num2;
+    return addResult;
+}
+//const finalResult = addition(12 + 13); // this is error input
+//const finalResult = addition(12, 13);
+//console.log(finalResult);
+
+
+
+//Multiplication
+
+function multiplication(num1, num2) {
+    const multResult = num1 * num2;
+    return multResult;
+}
+//const finalResult = multiplication(12 * 13); // this is error input
+const finalResult = multiplication(12, 13);
+console.log(finalResult);
