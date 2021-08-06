3ec7dfdcb2e0847ebdb4ef5cbf7ce73c161c5947
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-08-06 23:37:19 +0600

    Remove duplicate items from an array

---
+++ duplicate.js
@@ -0,0 +1,42 @@
+//Remove duplicate name
+
+const names = ['Rahim', 'Karim', 'Judu', 'Mudu', 'Kudu', 'Rahim', 'Alum', 'Judu', 'Kudu'];
+
+function removeDuplicate(names) {
+    const unique = [];
+    /* for (let i = 0; i < names.length; i++) {
+        const element = names[i];
+        console.log(element);
+    } */
+    for (const element of names) {
+        // console.log(element);
+        if (unique.indexOf(element) == -1) {
+            unique.push(element);
+        }
+    }
+    return unique;
+}
+
+const uniqueNames = removeDuplicate(names);
+//console.log(uniqueNames); 
+
+//Remove duplicate number
+const numbers = [1, 4, 6, 7, 9, 4, 8, 9, 7, 1, 2, 34, 78];
+
+function removeDuplicate(numbers) {
+    const unique = [];
+    /* for (let i = 0; i < names.length; i++) {
+        const element = names[i];
+        console.log(element);
+    } */
+    for (const element of numbers) {
+        console.log(element);
+        if (unique.indexOf(element) == -1) {
+            unique.push(element);
+        }
+    }
+    return unique;
+}
+
+const uniqueNumber = removeDuplicate(numbers);
+console.log(uniqueNumber);
